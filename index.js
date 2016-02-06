fcec02842429e845fafdd707429b8b49d3a4ec63
Daniel Sellers <daniel@designfrontier.net>
2016-02-06 16:00:47 -0700

    Initial commit and implementation for a simple timing harness
    Named hawking. Pass it the command and options you want to run
    and it will run them and then output the last thing written to
    stdout by the command. It also outputs the exit code, and the time
    that the command ran for in nanoseconds, milliseconds and seconds.
    
    Written for use in hacknightslc.com's prime number drag races.

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const args = process.argv
+    , cp = require('child_process')
+    , chalk = require('chalk')
+    , startTime = process.hrtime()
+    , runner = cp.spawn(args[2], args.slice(3));
+
+console.log(args[2], args.slice(3));
+
+let returnValue;
+
+runner.stdout.on('data', (data) => {
+    returnValue = data.toString('utf8').split('|').pop();
+});
+
+runner.stderr.on('data', (data) => {
+    console.log(chalk.red(data.toString('utf8')));
+});
+
+runner.on('close', (code) => {
+    const time = process.hrtime(startTime)
+        , nanos = time[0] * 1e9 + time[1]
+        , timeOutput = `\n ${nanos} nanoseconds
+ ${nanos / 1000000} milliseconds
+ ${nanos / 1000000000} seconds`;
+
+    console.log(`Command ran in: ${chalk.green(timeOutput)}`);
+    console.log(`Final Return: ${chalk.yellow(returnValue)}`);
+    console.log(`Exit Code Return: ${chalk.red(code)}`);
+});
